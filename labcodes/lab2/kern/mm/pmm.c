0fe6d2cddcb0976b5cd32a9f38bc71937c756039
Dstray <lixt12@qq.com>
2015-04-02 17:51:39 +0800

    lab1 & lab2

--- labcodes/lab2/kern/mm/pmm.c
+++ labcodes/lab2/kern/mm/pmm.c
@@ -20,7 +20,7 @@
  *   - add enough information to the TSS in memory as needed
  *   - load the TR register with a segment selector for that segment
  *
- * There are several fileds in TSS for specifying the new stack pointer when a
+ * There are several fields in TSS for specifying the new stack pointer when a
  * privilege level change happens. But only the fields SS0 and ESP0 are useful
  * in our os kernel.
  *
@@ -31,7 +31,7 @@
  * */
 static struct taskstate ts = {0};
 
-// virtual address of physicall page array
+// virtual address of physical page array
 struct Page *pages;
 // amount of physical memory (in pages)
 size_t npage = 0;
@@ -380,6 +380,17 @@ get_pte(pde_t *pgdir, uintptr_t la, bool create) {
     }
     return NULL;          // (8) return page table entry
 #endif
+    pde_t *pde_p = &pgdir[PDX(la)];
+    if (!(*pde_p & PTE_P)) { // 若該頁目錄項對應頁表未被建立
+        struct Page *page; // 爲該該頁表分配一個頁的空間
+        if (!create || (page = alloc_page()) == NULL)
+            return NULL;
+        set_page_ref(page, 1);
+        uintptr_t pa = page2pa(page); // 新頁表的物理基址
+        memset(KADDR(pa), 0, PGSIZE); // 初始化
+        *pde_p = pa | PTE_U | PTE_W | PTE_P; // 重置頁目錄項
+    }
+    return &((pte_t *)KADDR(PDE_ADDR(*pde_p)))[PTX(la)];
 }
 
 //get_page - get related Page struct for linear address la using PDT pgdir
@@ -425,6 +436,14 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
                                   //(6) flush tlb
     }
 #endif
+    if (*ptep & PTE_P) { // 確定該頁表項是否被建立
+    	struct Page *page = pte2page(*ptep); // 獲取對應物理頁
+    	page_ref_dec(page);
+    	if (page_ref(page) == 0)
+    		free_page(page); // 釋放這一物理頁
+    	*ptep = 0; // 重置頁表項
+    	tlb_invalidate(pgdir, la); // 刷新TLB
+    }
 }
 
 //page_remove - free an Page which is related linear address la and has an validated pte
